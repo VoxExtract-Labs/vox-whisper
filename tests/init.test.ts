9f44ec281f6512695367f05dd2ae682c248d2899
Angel S. Moreno <angelxmoreno@gmail.com>
2025-03-29 03:54:47 -0400

    chore: removed init test

--- tests/init.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'bun:test';
-import { initialized } from '@/index.ts';
-
-describe('Initial Test', () => {
-    it('is a place holder for now', () => {
-        expect(initialized).toBeTrue();
-    });
-});
